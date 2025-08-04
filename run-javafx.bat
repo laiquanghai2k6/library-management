bc9ee01a4f131977cf63f508aa9614e715d29cf1
Linh Huong <hoangthilinhhuong1@gmail.com>
2025-08-04 19:54:37 +0700

    setup sth

--- run-javafx.bat
+++ run-javafx.bat
@@ -7,15 +7,14 @@ for /R src %%f in (*.java) do (
     set SOURCES=!SOURCES! %%f
 )
 
-rem Biên dịch toàn bộ các file .java vào thư mục 'out'
-rem Đảm bảo rằng thư mục 'out' tồn tại trước khi biên dịch
-if not exist out mkdir out
-javac --module-path lib --add-modules javafx.controls ^
+rem Biên dịch toàn bộ
+javac --module-path lib --add-modules javafx.controls,javafx.fxml ^
 -cp "lib\postgresql-42.7.3.jar;lib\gson-2.10.1.jar" -d out %SOURCES%
 
-rem Chạy chương trình JavaFX
-rem Đảm bảo tên class chính (Main Class) là 'view.TestJavaFX'
-java --module-path lib --add-modules javafx.controls ^
--cp "out;lib\postgresql-42.7.3.jar;lib\gson-2.10.1.jar" view.TestJavaFX
+xcopy /Y /S src\view\*.fxml out\view\
+
+rem Chạy chương trình
+java --module-path lib --add-modules javafx.controls,javafx.fxml ^
+-cp "out;lib\postgresql-42.7.3.jar;lib\gson-2.10.1.jar" app.Main
 
 pause
--- src/view/UIController.java
+++ src/view/UIController.java
@@ -1,5 +1,44 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class UIController {
-    
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void loadUserView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/view/UserView.fxml"));
+        switchScene(event, root);
+    }
+
+    public void loadDocumentView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/view/DocumentView.fxml"));
+        switchScene(event, root);
+    }
+
+    public void loadBorrowView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/view/BorrowView.fxml"));
+        switchScene(event, root);
+    }
+
+    public void loadReturnView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/view/ReturnView.fxml"));
+        switchScene(event, root);
+    }
+
+    private void switchScene(ActionEvent event, Parent view) {
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(view);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
