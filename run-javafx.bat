d19608a18631176ee5ccf9cd8fb057884141d213
laiquanghai2k6 <yortvrluer@gmail.com>
2025-08-11 17:00:38 +0700

    fix: fix assets and css files

--- run-javafx.bat
+++ run-javafx.bat
@@ -11,7 +11,12 @@ rem Biên dịch toàn bộ
 javac --module-path lib --add-modules javafx.controls,javafx.fxml ^
 -cp "lib\postgresql-42.7.3.jar;lib\gson-2.10.1.jar" -d out %SOURCES%
 
+rem Copy tất cả assets trong src sang out\view
+xcopy /Y /I /E src\assets out\assets
+rem Copy tất cả file .fxml trong src\view sang out\view
 xcopy /Y /S src\view\*.fxml out\view\
+rem Copy thêm file CSS (hoặc bạn có thể copy tất cả resource cùng lúc)
+xcopy /Y /I /E src\view\*.css out\view\
 
 rem Chạy chương trình
 java --module-path lib --add-modules javafx.controls,javafx.fxml ^
